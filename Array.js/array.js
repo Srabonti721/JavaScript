63c714eb9a4ddc4b52c2974636857f75132fae28
Srabonti721 <somayaaktarshabonti721@gmail.com>
2026-03-01 00:12:20 +0600

    added array lenght index property

---
+++ Array.js/array.js
@@ -0,0 +1,15 @@
+const name = ["riya", "rubina", "halima", "hemu", "junaki", "ferthose", "sima", "piyaju"];
+const number = [77, 22, 34, 99, 12, 56, 24, 78, 95, 11, 25, 86];
+const ispased = [true, false, true, false, false, true];
+const mixedArray = ["tonu", 32, 45, false, "minal", 56, true];
+// console.log(number.length);
+// console.log(mixedArray[3]);
+console.log(name);
+console.log(name[4]);
+// array get
+ const fifth = name[4];
+ console.log(fifth);
+//  set 
+name[1] = "talha";
+console.log(name);
+
---
+++ Array.js/index.js
@@ -0,0 +1,14 @@
+const number = [22, 31, 34, 77, 28, 32, 99];
+// console.log(number.length);
+// console.log(number[3]);
+// get
+// const num1 = number[1];
+// console.log(num1);
+// set
+// number[0] = 100;
+// console.log(number);
+
+const price = [250, 400, 100, 500, 300, 110]
+console.log(price.length);
+price[6] = 310;
+console.log(price);
