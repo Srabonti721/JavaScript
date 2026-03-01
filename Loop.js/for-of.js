a8fab33a5a6d8825ec6f6a5f15d541e5fd21bbe2
Srabonti721 <somayaaktarshabonti721@gmail.com>
2026-03-02 00:19:47 +0600

    added for of loop

---
+++ Loop.js/for-of.js
@@ -0,0 +1,16 @@
+const fruits = ["apple", "orange", "watermelon", "jackfruits", "grape", "banana"] ;
+for(const fruit of fruits){
+    console.log(fruit);  
+}
+
+const numbers = [ 12, 76, 23, 87, 95, 99, 45, 26, 30];
+for(const number of numbers){
+    console.log(number);
+    
+}
+
+const ispassed = [true, false, true, true]
+for(const passed of ispassed){
+    console.log(passed);
+    
+}
