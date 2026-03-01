689db40f74c2c5ce5a9a435038672d37417a3417
Srabonti721 <somayaaktarshabonti721@gmail.com>
2026-03-02 00:12:48 +0600

    recat variable, condition, array

---
+++ Loop.js/recap.js
@@ -0,0 +1,15 @@
+// const : Value will not change.
+// let : Value can change.
+// var : we should not used.
+
+// variable 
+const amount = 500;
+// condition
+if(amount>700){
+
+}
+else{
+
+}
+// array
+const numbers = [11, 22, 33, 44, 55, 66 ]
