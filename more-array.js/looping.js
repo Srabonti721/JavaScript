fdcc4ad16b5400524728c94751805baf6e86b634
Srabonti721 <somayaaktarshabonti721@gmail.com>
2026-03-11 10:12:41 +0600

    added more of array

---
+++ more-array.js/looping.js
@@ -0,0 +1,28 @@
+const friends = ["monami", "sujaita", "sanjida","subro", "ove"];
+for(const friend of friends){
+    // console.log(friend);
+}
+
+for (let i = 0; i<friends.length ; i++){
+    // console.log(i);
+    // console.log(friends[i]);
+}
+ let j = 0;
+ while(j<friends.length){
+    // console.log(friends[j]);
+    j++;
+ }
+ const numbers = [ 12, 234, 132, 345, 45657, 67, 356, 612];
+ for(let num = 0; num<numbers.length; num++){
+    // console.log(numbers[num]);
+ }
+
+ for (const n of numbers ){
+    // console.log(n); 
+ }
+  let sonkha = 0;
+  while(sonkha<numbers.length){
+    console.log(sonkha);
+     console.log(numbers[sonkha]);
+     sonkha++;
+  }
