117dc441d4b13919a26941bdbd343e4dc8f772c7
Srabonti721 <somayaaktarshabonti721@gmail.com>
2026-03-13 22:32:12 +0600

    added function syntax

---
+++ function.js/function.js
@@ -0,0 +1,35 @@
+const number = 24;
+function pen (){
+    console.log("this is a new pen");
+    console.log("this is a old pen");
+    console.log("this is a new old pen");
+    console.log("this is my favoret pen");
+}
+ pen();
+
+/*
+ * function declaration ar structure 
+function key word function ar name 1st braket 2nd braket .function call.
+
+
+function name () {
+// body
+}
+name();
+
+
+ */  
+ function readBook (){
+    console.log("boi ar kase jau");
+    console.log("posonder boi hate naw");
+    console.log("boi niya table a boso");
+    console.log("akhon boi porta thako");
+ }
+ readBook ();
+  
+//  loop and function ar maja difrent . loop command onujaye kaj ses kora. function ter kaj ses kora ono kaj cora aber o call korala oi kaj kora.
+ console.log("+++++++");
+ console.log("---------");
+ readBook();
+ 
+ 
