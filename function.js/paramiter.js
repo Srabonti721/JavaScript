c5a9b249ea87a978028117c30c72d64889b81b46
Srabonti721 <somayaaktarshabonti721@gmail.com>
2026-03-14 00:24:06 +0600

    added paramiter

---
+++ function.js/paramiter.js
@@ -0,0 +1,30 @@
+function square (number){
+console.log("value of the number is paramiter : ", number);
+const borgo = number*number;
+console.log("value of the number is borgo: ", borgo);
+
+}
+square(4);
+// square(5);
+// square(2);
+// square(9);
+
+function sum (num1, num2){
+    const total = num1 + num2;
+    console.log("total sum is :", total);   
+}
+sum(4, 12);
+// sum(77, 45);
+ 
+function addAll (a,b,c,d){
+    const multiple = a*b*c*d;
+    console.log(a,b,c,d);
+    console.log(multiple);  
+}
+addAll(2, 1, 3);
+
+function sumAll (a,b,c,d,e){
+const totalSum = a+b+d+c+e;
+console.log(totalSum);
+}
+sumAll(2,8,7,6,4);
